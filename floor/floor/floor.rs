6036de605f3de9aadefa2e3453870e3fe9ecd06c
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2024-08-30 23:11:09 -0600

    Updates

--- floor/floor/floor.rs
+++ floor/floor/floor.rs
@@ -110,7 +110,7 @@ extern "C" {
 #[no_mangle]
 fn musl_rust() {
     let x = 0.0f64.verifier_nondet();
-    verifier_assume!(!x.verifier_is_nan());
+    // verifier_assume!(!x.verifier_is_nan());
     let y = unsafe { musl_floor(x) };
     let z = rust_floor(x);
     verifier_assert!(y == z);
@@ -119,7 +119,7 @@ fn musl_rust() {
 #[no_mangle]
 fn musl_smack() {
     let x = 0.0f64.verifier_nondet();
-    verifier_assume!(!x.verifier_is_nan());
+    // verifier_assume!(!x.verifier_is_nan());
     let y = unsafe { musl_floor(x) };
     let z = unsafe { floor(x) };
     verifier_assert!(y == z);
@@ -128,7 +128,7 @@ fn musl_smack() {
 #[no_mangle]
 fn rust_smack() {
     let x = 0.0f64.verifier_nondet();
-    verifier_assume!(!x.verifier_is_nan());
+    // verifier_assume!(!x.verifier_is_nan());
     let y = rust_floor(x);
     let z = unsafe { floor(x) };
     verifier_assert!(y == z);
